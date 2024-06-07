86661e3a013fc30f96876f2fdcef1f52bd1afc45
igortice <igortice@gmail.com>
2024-06-07 21:39:10 +0100

    improves input message

--- src/screens/App/Chat/components/FooterInput/index.tsx
+++ src/screens/App/Chat/components/FooterInput/index.tsx
@@ -1,38 +1,114 @@
 import { Ionicons } from '@expo/vector-icons';
 import { Box, Button, HStack, KeyboardAvoidingView } from '@gluestack-ui/themed';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import Animated, {
+  FadeIn,
+  runOnJS,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { StyledBlurView, StyledInputMessage } from '@/components';
 import { Colors } from '@/configs';
 
+const DURATION = 100;
+const DELAY = 100;
+
 export const FooterInput = () => {
-  const inputRef = useRef<any>(null);
   const { bottom } = useSafeAreaInsets();
+  const inputRef = useRef<any>(null);
+  const [showActionsButtons, setShowActionsButtons] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>('');
+
+  const opacityCamera = useSharedValue<number>(0);
+  const opacityImage = useSharedValue<number>(0);
+  const opacityFolder = useSharedValue<number>(0);
+  const opacityAdd = useSharedValue<number>(1);
+
+  const handleShowActionsButtons = () => {
+    opacityAdd.value = withDelay(
+      0 * DELAY,
+      withTiming(0, { duration: DURATION }, finished => {
+        if (finished) runOnJS(setShowActionsButtons)(true);
+      }),
+    );
+    opacityCamera.value = withDelay(1 * DELAY, withTiming(1, { duration: DURATION }));
+    opacityImage.value = withDelay(2 * DELAY, withTiming(1, { duration: DURATION }));
+    opacityFolder.value = withDelay(3 * DELAY, withTiming(1, { duration: DURATION }));
+  };
+
+  const handleHideActionsButtons = () => {
+    opacityFolder.value = withDelay(0 * (DELAY / 2), withTiming(0, { duration: DURATION }));
+    opacityImage.value = withDelay(1 * (DELAY / 2), withTiming(0, { duration: DURATION }));
+    opacityCamera.value = withDelay(2 * (DELAY / 2), withTiming(0, { duration: DURATION }));
+    opacityAdd.value = withDelay(
+      3 * (DELAY / 2),
+      withTiming(1, { duration: DURATION }, finished => {
+        if (finished) {
+          runOnJS(setShowActionsButtons)(false);
+        }
+      }),
+    );
+  };
 
   return (
     <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={70}>
       <StyledBlurView intensity={90} tint="extraLight" pb={bottom} pt={12} px={12}>
         <HStack space="xs" flexDirection="row" justifyContent="space-between" alignItems="center">
-          <Box>
-            <Button variant="link" $active-opacity={0.6} onPress={() => console.log('emoji')}>
-              <Ionicons name="add-circle" size={38} color={Colors.greyLight} />
-            </Button>
-          </Box>
+          <HStack space="md">
+            {(showActionsButtons && (
+              <>
+                <Animated.View style={{ opacity: opacityCamera }}>
+                  <Button variant="link" $active-opacity={0.6} onPress={() => console.log('emoji')}>
+                    <Ionicons name="camera-outline" size={30} color={Colors.grey} />
+                  </Button>
+                </Animated.View>
+
+                <Animated.View style={{ opacity: opacityImage }}>
+                  <Button variant="link" $active-opacity={0.6} onPress={() => console.log('emoji')}>
+                    <Ionicons name="image-outline" size={30} color={Colors.grey} />
+                  </Button>
+                </Animated.View>
+
+                <Animated.View style={{ opacity: opacityFolder }}>
+                  <Button variant="link" $active-opacity={0.6} onPress={() => console.log('emoji')}>
+                    <Ionicons name="folder-outline" size={30} color={Colors.grey} />
+                  </Button>
+                </Animated.View>
+              </>
+            )) || (
+              <Animated.View style={{ opacity: opacityAdd }}>
+                <Button variant="link" $active-opacity={0.6} onPress={handleShowActionsButtons}>
+                  <Ionicons name="add-circle" size={34} color={Colors.grey} />
+                </Button>
+              </Animated.View>
+            )}
+          </HStack>
 
           <Box flex={1}>
-            <StyledInputMessage ref={inputRef} placeholder="Message" multiline />
+            <StyledInputMessage
+              onPressIn={handleHideActionsButtons}
+              ref={inputRef}
+              value={message}
+              onChangeText={setMessage}
+              placeholder="Message"
+              multiline
+            />
           </Box>
 
-          <Box>
-            <Button
-              rounded="$full"
-              variant="link"
-              $active-opacity={0.6}
-              onPress={() => console.log('send')}>
-              <Ionicons name="arrow-up-circle-sharp" size={38} color={Colors.black} />
-            </Button>
-          </Box>
+          {message?.length > 0 && (
+            <Animated.View entering={FadeIn}>
+              <Button
+                rounded="$full"
+                variant="link"
+                $active-opacity={0.6}
+                onPress={() => console.log('send')}>
+                <Ionicons name="arrow-up-circle" size={34} color={Colors.grey} />
+              </Button>
+            </Animated.View>
+          )}
         </HStack>
       </StyledBlurView>
     </KeyboardAvoidingView>
